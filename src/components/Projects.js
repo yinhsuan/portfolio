d2a592f378169dc9f2ca7bb50777e4e3491dc619
yinhsuan <sandy10235168@gmail.com>
2023-05-10 21:45:20 +0800

    disable tabs in Projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -52,7 +52,20 @@ export const Projects = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
                 <h2>Projects</h2>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                <Row>
+                  {
+                    projects.map((project, index) => {
+                      return (
+                        <ProjectCard
+                          key={index}
+                          {...project}
+                          />
+                      )
+                    })
+                  }
+                </Row>
+                
+                {/* <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
                       <Nav.Link eventKey="first">Tab 1</Nav.Link>
@@ -86,7 +99,7 @@ export const Projects = () => {
                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
                     </Tab.Pane>
                   </Tab.Content>
-                </Tab.Container>
+                </Tab.Container> */}
               </div>}
             </TrackVisibility>
           </Col>
