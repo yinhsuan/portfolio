d045d103cb91479fafd337e57475784b7c23ea7a
yinhsuan <sandy10235168@gmail.com>
2023-05-11 12:28:24 +0800

    edit the overall layout

--- src/components/About.js
+++ src/components/About.js
@@ -25,7 +25,7 @@ export const About = () => {
                 <TrackVisibility>
                 {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                    <div style={{ marginLeft:'100px', marginTop:"50px" }}>
+                    <div>
                         <h2>About</h2>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                     </div>
