bb77a8513905ab8143638b864a32de6a699615db
yinhsuan <sandy10235168@gmail.com>
2023-05-11 11:33:29 +0800

    edit the font size of rendered text

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -59,7 +59,7 @@ export const Banner = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
                 <h1>{`Hi! I'm Sandy`}</h1>
-                  <h1> <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
+                  <h2> <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h2>
                   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                   <button onClick={() => console.log('connect')}>More about Me<ArrowRightCircle size={25} /></button>
                   {/* <a href="#about"><button onClick={() => console.log('connect')}>More about Me<ArrowRightCircle size={25} /></button></a> */}
