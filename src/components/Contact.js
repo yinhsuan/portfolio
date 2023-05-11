0ecae7efa869567b376d275d9a4e40bd5407155f
yinhsuan <sandy10235168@gmail.com>
2023-05-11 10:03:04 +0800

    make the field shorter in Contact Me

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -67,33 +67,35 @@ export const Contact = () => {
             <TrackVisibility>
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <h2>Contact Me</h2>
-                <form onSubmit={handleSubmit}>
-                  <Row>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="text" value={formDetails.lasttName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)}/>
-                    </Col>
-                    <Col size={12} className="px-1">
-                      <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
-                      <button type="submit"><span>{buttonText}</span></button>
-                    </Col>
-                    {
-                      status.message &&
-                      <Col>
-                        <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
+                  <div style={{ width: "90%", marginLeft: 'auto', marginRight: 'auto' }}>
+                  <h2>Contact Me</h2>
+                  <form onSubmit={handleSubmit}>
+                    <Row>
+                      <Col size={12} sm={6} className="px-1">
+                        <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
                       </Col>
-                    }
-                  </Row>
-                </form>
+                      <Col size={12} sm={6} className="px-1">
+                        <input type="text" value={formDetails.lasttName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
+                      </Col>
+                      <Col size={12} sm={6} className="px-1">
+                        <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
+                      </Col>
+                      <Col size={12} sm={6} className="px-1">
+                        <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)}/>
+                      </Col>
+                      <Col size={12} className="px-1">
+                        <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
+                        <button type="submit"><span>{buttonText}</span></button>
+                      </Col>
+                      {
+                        status.message &&
+                        <Col>
+                          <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
+                        </Col>
+                      }
+                    </Row>
+                  </form>
+                  </div>
               </div>}
             </TrackVisibility>
           </Col>
