49c0d727a2bb7ebc7dcb8535c70143ffe76032dc
yinhsuan <sandy10235168@gmail.com>
2023-07-01 14:54:10 +0800

    add project content

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const App = () => {
       <About />
       {/* <Skills /> */}
       <Projects />
-      <Blog />
+      {/* <Blog /> */}
       <Contact />
       <Footer />
     </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,16 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import projImg1 from "../assets/img/project-img1.png";
-import projImg2 from "../assets/img/project-img2.png";
-import projImg3 from "../assets/img/project-img3.png";
+// import projImg1 from "../assets/img/project-img1.png";
+// import projImg2 from "../assets/img/project-img2.png";
+// import projImg3 from "../assets/img/project-img3.png";
+
+import projImg1 from "../assets/img/project1.png";
+import projImg2 from "../assets/img/project2.png";
+import projImg3 from "../assets/img/project3.png";
+import projImg4 from "../assets/img/project4.png";
+import projImg5 from "../assets/img/project5.png";
+import projImg6 from "../assets/img/project6.png";
+
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -28,17 +36,17 @@ export const Projects = () => {
     {
       title: "Business Startup",
       description: "Design & Development",
-      imgUrl: projImg1,
+      imgUrl: projImg4,
     },
     {
       title: "Business Startup",
       description: "Design & Development",
-      imgUrl: projImg2,
+      imgUrl: projImg5,
     },
     {
       title: "Business Startup",
       description: "Design & Development",
-      imgUrl: projImg3,
+      imgUrl: projImg6,
     },
   ];
 
@@ -48,7 +56,7 @@ export const Projects = () => {
         <Row>
           <Col size={12}>
             <h2>Projects</h2>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            <p>As a skilled Web Developer specializing in network technologies, I possess expertise in C, C++, JavaScript, Python, SDN, and more. My portfolio showcases my proficiency in web development, demonstrating my strong knowledge of networking principles. I have successfully completed projects in various domains, reflecting my ability to apply my skills to real-world scenarios</p>
             {/* <TrackVisibility>
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn": ""}> */}
